56069ef80dce9f4ec5adca388f50f34497db2f9b
Dushantha Olinda <2020cs185@stu.ucsc.cmb.ac.lk>
2023-02-28 10:34:33 +0530

    Blood Request table

--- db/Blood_Requests.sql
+++ db/Blood_Requests.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Feb 27, 2023 at 08:55 AM
+-- Generation Time: Feb 28, 2023 at 05:04 AM
 -- Server version: 5.7.39
 -- PHP Version: 7.4.33
 
@@ -35,7 +35,7 @@ CREATE TABLE `Blood_Requests` (
   `Type` int(11) NOT NULL DEFAULT '1',
   `Status` int(11) NOT NULL DEFAULT '1',
   `Quantity` int(11) NOT NULL,
-  `Remark` varchar(100) DEFAULT NULL
+  `Remark` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -43,12 +43,11 @@ CREATE TABLE `Blood_Requests` (
 --
 
 INSERT INTO `Blood_Requests` (`Request_ID`, `Requested_By`, `BloodGroup`, `Requested_At`, `Type`, `Status`, `Quantity`, `Remark`) VALUES
-('Req_01', 'Hos_01', 'A+', '2023-02-23 05:09:35', 1, 1, 2, 'This is a remark'),
-('Req_02', 'Hos_01', 'O-', '2023-02-23 05:13:32', 1, 1, 2, NULL),
-('Req_03', 'Hos_01', 'O+', '2023-02-23 05:14:46', 2, 2, 1, 'This is a remark'),
-('Req_04', 'Hos_01', 'AB+', '2023-02-23 05:15:14', 2, 3, 4, NULL),
-('Req_4787', 'Hos_01', 'A+', '2023-02-27 03:18:51', 2, 1, 7, 'dfg'),
-('Req_8218', 'Hos_01', 'AB-', '2023-02-27 03:24:06', 2, 1, 1, 'Uberta le denne moda Hutta');
+('Req_490', 'Hos_01', 'A+', '2023-02-27 03:41:00', 2, 1, 1, 'Hadissi ikmanata ewapan'),
+('Req_5500', 'Hos_02', 'A+', '2023-02-27 08:00:34', 1, 1, 2, 'Le madi tikak ewapan'),
+('Req_558', 'Hos_02', 'A+', '2023-02-27 08:00:18', 2, 1, 1, 'mathak wenne hutta'),
+('Req_722', 'Hos_02', 'A+', '2023-02-27 08:01:01', 2, 1, 5, 'anam manam wana le thamai onaa'),
+('Req_7613', 'Hos_01', 'A+', '2023-02-27 03:41:24', 1, 1, 6, 'Marenna tharam amaru un ne the ekak bila ehema ewapan');
 
 --
 -- Indexes for dumped tables
