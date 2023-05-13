d9b2a876489870145c4d1250805323f2cdbbac18
Dinushan <2020cs202@stu.ucsc.cmb.ac.lk>
2023-05-13 23:46:56 +0530

    Changed Country Code

--- core/Model.php
+++ core/Model.php
@@ -79,7 +79,7 @@ abstract class Model
                 if ($rulename===self::RULE_NUMERIC && !is_numeric($value))
                 {
                     $this->addErrorRule($attribute, self::RULE_NUMERIC);
-                }if ($rulename===self::RULE_MOBILE_NO && !preg_match('/^(?:0|94|\+94)?(?:(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\d)\d{6}$/', $value))
+                }if ($rulename===self::RULE_MOBILE_NO && !preg_match('/^(?:0|94|\+94|0094)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|91)([0234579])|7([01245678])\d)\d{6}$/', $value))
                 {
                     $this->addErrorRule($attribute, self::RULE_MOBILE_NO);
                 }
