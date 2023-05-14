7e456cd9dc7c6a5122eeca379603afaf899ffb47
Dinushan <2020cs202@stu.ucsc.cmb.ac.lk>
2023-05-15 03:53:16 +0530

    Final Change t-1

--- app/view/pages/Admin/manageBank.php
+++ app/view/pages/Admin/manageBank.php
@@ -18,7 +18,7 @@
         </div>
     </div>
     <div class="d-flex w-95 pt-2 pb-1 flex-center flex-column bg-white border-radius-10" style="max-height: 80vh">
-        <div class="w-95  d-flex min-h-75 overflow-y-overlay">
+        <div class="w-95  d-flex min-h-75 overflow-y-overlay" id="div1">
             <table class="" id="bankTable">
                 <thead class="sticky top-0">
                 <tr>
@@ -87,4 +87,5 @@
         </div>
     </div>
 </div>
+
 
--- app/view/pages/Authentication/UserRegister.php
+++ app/view/pages/Authentication/UserRegister.php
@@ -26,7 +26,7 @@ FlashMessage::RenderFlashMessages();
         <div class="d-flex flex-column flex-center w-70 gap-1">
             <div class="text-2xl d-flex flex-center gap-1 d-flex mb-1 font-bold text-dark w-100 text-center">
                 <img src="/public/icons/RegisterUserIcon.png" alt="" width="60">
-                <div class="text-center"><?= $role ?> Registration</div>
+                <div class="text-center text-4xl" style="rgba(255,255,255,0.5)"><?= $role ?> Registration</div>
             </div>
             <input type="hidden" name="role" value="<?=$role?>">
             <div class="form-group">
