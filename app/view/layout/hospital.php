28e3ee4d0e6eabb28d2104b75604e4d64b7471af
Dushantha Olinda <2020cs185@stu.ucsc.cmb.ac.lk>
2023-05-15 06:17:14 +0530

    password change

--- app/view/layout/hospital.php
+++ app/view/layout/hospital.php
@@ -89,8 +89,7 @@
                                 <div class="d-flex w-100 justify-content-center">
                                     <img id="profileImage" src="<?= $User->getProfileImage()?>" alt="" width=300px height=300px class="border-1 border-radius-50 " style="object-fit:cover"/>
                                  </div>
-                                 <button class="btn btn-success d-flex align-items-center font-bold" onclick="ChangeProfileImage()"><span><img src="/public/icons/camera.svg" alt="" width="24px" class="cursor invert-100" onclick="EditProfile()"></span> &nbsp;Change Profile Image</button>
-
+                                 
                                 <div class="bg-dark d-flex justify-content-center w-100 text-center text-white py-0-5 px-1 relative">
                                     <div class="d-flex flex-center py-0-5"> Profile Details</div>
                                 </div>
@@ -118,6 +117,122 @@
                        </div>
                 </div>
             `,
+        })
+    }
+
+    const ChangePassword = ()=>{
+        OpenDialogBox({
+            id: 'changePassword',
+            title: 'Change Password',
+            popupOrder: 1,
+            content: `
+                <div class="d-flex flex-column gap-1 w-100">
+                     <div id="changePassword" class="d-flex flex-column gap-1">
+                         <div class="form-group">
+                            <label for="oldPassword" class="w-50">Old Password</label>
+                            <div class="d-flex flex-column w-50">
+                                 <input type="password" class="form-control w-100" min="8" max="30" id="oldPassword" placeholder="Enter Old Password">
+                                 <span class="text-danger" style="margin-top: 0.5rem" id="CurrentPasswordError"> </span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="newPassword" class="w-50">New Password</label>
+                            <div class="d-flex flex-column w-50">
+                                 <input type="password" class="form-control w-100" min="8" max="30" id="newPassword" placeholder="Enter New Password">
+                                 <span class="text-danger" style="margin-top: 0.5rem" id="NewPasswordError"> </span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="confirmPassword" class="w-50">Confirm Password</label>
+                            <div class="d-flex flex-column w-50">
+                                 <input type="password" class="form-control w-100" min="8" max="30" id="confirmPassword" placeholder="Enter Confirm Password">
+                                 <span class="text-danger" style="margin-top: 0.5rem" id="ConfirmPasswordError"> </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>`,
+            successBtnText: 'Change Password',
+            successBtnAction: ()=>{
+                const url ='/hospital/changePassword';
+                const oldPassword = document.querySelector('#oldPassword').value;
+                const newPassword = document.querySelector('#newPassword').value;
+                const confirmPassword = document.querySelector('#confirmPassword').value;
+                if (oldPassword.length === 0){
+                    document.querySelector('#CurrentPasswordError').innerText = 'Old Password is Required';
+                    return;
+                }
+                if (newPassword.length === 0){
+                    document.querySelector('#NewPasswordError').innerText = 'New Password is Required';
+                    return;
+                }
+                if (confirmPassword.length === 0){
+                    document.querySelector('#ConfirmPasswordError').innerText = 'Confirm Password is Required';
+                    return;
+                }
+                if (newPassword.length < 8){
+                    document.querySelector('#NewPasswordError').innerText = 'Password must be at least 8 characters';
+                    return;
+                }
+                if (newPassword.length > 30){
+                    document.querySelector('#NewPasswordError').innerText = 'Password must be at most 30 characters';
+                    return;
+                }
+                if (confirmPassword.length < 8){
+                    document.querySelector('#ConfirmPasswordError').innerText = 'Password must be at least 8 characters';
+                    return;
+                }
+                if (confirmPassword.length > 30){
+                    document.querySelector('#ConfirmPasswordError').innerText = 'Password must be at most 30 characters';
+                    return;
+                }
+                if (newPassword !== confirmPassword){
+                    document.querySelector('#ConfirmPasswordError').innerText = 'Confirm Password is not Matched';
+                    return;
+                }
+
+                const formData = new FormData();
+                formData.append('CurrentPassword',oldPassword);
+                formData.append('NewPassword',newPassword);
+                formData.append('ConfirmPassword',confirmPassword);
+                fetch(url, {
+                    method: 'POST',
+                    body: formData
+                }).then(response => response.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.status){
+                            ShowToast({
+                                title: 'Success',
+                                message: 'Password Changed Successfully',
+                                type: 'success',
+                                duration: 3000
+                            });
+                            CloseDialogBox('changePassword');
+                        }else{
+                            ShowToast({
+                                title: 'Error',
+                                message: data.message,
+                                type: 'danger',
+                                duration: 3000
+                            });
+                            const Filed = data.field;
+                            const CurrentPasswordInput = document.querySelector('#oldPassword');
+                            const NewPasswordInput = document.querySelector('#newPassword');
+                            const ConfirmPasswordInput = document.querySelector('#confirmPassword');
+
+                            if (Filed === 'CurrentPassword'){
+                                CurrentPasswordInput.classList.add('border-danger');
+                                document.querySelector('#CurrentPasswordError').innerHTML = data.message;
+                            }else if (Filed === 'NewPassword'){
+                                NewPasswordInput.classList.add('border-danger');
+                                document.querySelector('#NewPasswordError').innerHTML = data.message;
+                            }else if (Filed === 'ConfirmPassword'){
+                                ConfirmPasswordInput.classList.add('border-danger');
+                                document.querySelector('#ConfirmPasswordError').innerHTML = data.message;
+                            }
+                        }
+                    })
+            }
         })
     }
 
